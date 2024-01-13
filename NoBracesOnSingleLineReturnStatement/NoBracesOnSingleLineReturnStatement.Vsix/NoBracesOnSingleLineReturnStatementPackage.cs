f01d9427cafbca95c8db6abe2ef62fbe22a0b56d
calledude <22471295+calledude@users.noreply.github.com>
2024-01-13 19:24:05 +0100

    Add package bootstrapper, update vsixmanifest

---
+++ NoBracesOnSingleLineReturnStatement/NoBracesOnSingleLineReturnStatement.Vsix/NoBracesOnSingleLineReturnStatementPackage.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Threading;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NoBracesOnSingleLineReturnStatement.Vsix
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [Guid("95eb0ac7-0e3c-463f-830e-d00e2b8e78bc")]
+    public sealed class NoBracesOnSingleLineReturnStatementPackage : AsyncPackage
+    {
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+            => await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+    }
+}
